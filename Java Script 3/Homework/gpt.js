72b3bd6719cf9913812f15d53a2725c35a5e0623
Vako Kapanadze <vako.kapa@gmail.com>
2025-02-10 15:51:48 +0400

    Homework part 3

--- Java Script 3/Homework/gpt.js
+++
@@ -1,4 +0,0 @@
-// Task 1: Basic Array Manipulation
-let numbers = [10, 20, 30, 40, 50];
-console.log[number[0]]
-console.log[numbers.length-1]
---
+++ Java Script 3/Homework/second.js
@@ -0,0 +1,31 @@
+// // Task 1: Basic Array Manipulation
+// let numbers = [10, 20, 30, 40, 50];
+// console.log(numbers[0])
+// console.log(numbers[numbers.length-1])
+
+// // Task 2: Find Even and Odd Numbers
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i<nums.length; i++){
+//     if (nums[i]%2 ==0){
+//         console.log(nums[i])
+//     }
+// }
+
+// // Task 3: Check for Specific Value
+// let numbers = [5, 10, 25, 30, 40, 50];
+// for (let i = 0; i<numbers.length; i++){
+//     if (numbers[i] == 25){
+//         console.log("Yes there is a number:", numbers[i])
+//     }
+// }
+
+// Task 4: Sum of All Elements in an Array
+// let nums = [1, 2, 3, 4, 5];
+// let sum = 0;
+
+// for (let i = 0; i < nums.length; i++) {
+//     sum += nums[i]; 
+// }
+
+// console.log(sum);
+
