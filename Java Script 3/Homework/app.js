13310275bd9b14b640dcaa32674d023bee2d09d7
Vako Kapanadze <vako.kapa@gmail.com>
2025-02-10 15:21:40 +0400

    Homework part 2

---
+++ Java Script 3/Homework/app.js
@@ -0,0 +1,44 @@
+// // დავალება N1
+// let UserNum = prompt("Hello, Please enter number from 50 to 100")
+// if (!isNaN(UserNum) && !isNaN(UserNum)){
+//     if (UserNum>=50 && UserNum<=100){
+//         for (let i = 50; i<UserNum; i+=3)
+//             console.log(i)
+//     }
+//     else{
+//         console.log("Please enter number that is bigger than 50 and lower than 100")
+//     }
+// }
+// else{
+//     console.log("Please Enter only Numbers")
+// }
+// დავალება N2
+// let NumArr = [27, 13, 61, 83, 10, 32];
+// let find; 
+// for (let i = 0; i < NumArr.length; i++) {
+//     if (NumArr[i] == 10) {
+//         find = true;
+//     }
+// }
+
+// if (find == true) {
+//     console.log(true); 
+// } else {
+//     console.log(false);
+// }
+
+// // დავალება N3
+// let StrArr = ["Mike", "Jerry", "Dave", "Roy"];
+
+// for (let i = 0; i < StrArr.length; i++) {
+//     console.log(StrArr[i][0]);
+// }
+
+// დავალება N4
+// let ContArr = [5, 12, 25, 2, 55, 150, 10]
+// for (let i = 0; i < ContArr.length; i++) {
+//     if (ContArr[i] > 10 && ContArr[i] < 100){
+//         console.log(ContArr[i])
+//     }
+// }
+
---
+++ Java Script 3/Homework/gpt.js
@@ -0,0 +1,4 @@
+// Task 1: Basic Array Manipulation
+let numbers = [10, 20, 30, 40, 50];
+console.log[number[0]]
+console.log[numbers.length-1]
