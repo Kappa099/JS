849508c4361ba548ddf51b617e8aeec63eadd564
V.Kapanadze <vako.kapa@gmail.com>
2025-11-04 19:53:26 +0400

    almost

--- Java Script 11/Colorss/code.js
+++ Java Script 11/Colorss/code.js
@@ -2,6 +2,10 @@ let color = document.querySelector('.color')
 let options = document.querySelector('.options')
 let message = document.querySelector("#message");
 message.textContent = "Guess the color!";
+let stats = document.querySelector(".stats")
+let time = document.querySelector(".time")
+let rounds = document.querySelector(".round")
+let score = document.querySelector(".score")
 
 function getRandomRGB() {
   const r = Math.round(Math.random() * 255);
@@ -15,41 +19,61 @@ function setTargetColor() {
     color.style.backgroundColor = target
     return target
 }
-
-
-function rounds() {
-    let buttonCount = 4;
-    if (round >= 15) {
+let round = 1
+function getButtonCount(round) {
+  let buttonCount = 4;
+  if (round >= 15) {
     buttonCount = 10;
-    } else if (round >= 10) {
+  } else if (round >= 10) {
     buttonCount = 8;
-    } else if (round >= 5) {
+  } else if (round >= 5) {
     buttonCount = 6;
-    }
+  }
+  return buttonCount;
 }
+
 function setButtons(target, count) {
     options.innerHTML = '';
-    for (let i = 0; i < 4; i++){
-    let button = document.createElement("button");
-    button.classList.add("btn");
-    options.appendChild(button);
+    let correctIndex = Math.floor(Math.random() * count);
+    for (let i = 0; i < count; i++){
+        let button = document.createElement("button");
+        button.classList.add("btn");
+        options.appendChild(button);
 
-    let index = Math.floor(Math.random()* options.children.length)
-        btn.addEventListener('click', function (){
-            if (btn.style.backgroundColor == target){
+        if (i === correctIndex) {
+        button.style.backgroundColor = target;
+        } else {
+        button.style.backgroundColor = getRandomRGB();
+        }
+
+        button.addEventListener('click', function (){
+            if (button.style.backgroundColor == target){
             message.textContent = "Your answer is Correct";
+            message.style.backgroundColor = "green"
+
             }
             else {
             message.textContent = "Your answer is Wrong!";
+            message.style.backgroundColor = "red"
             }
+            round++;
             let newTarget = setTargetColor()
-            setButtons(newTarget)
+            let newCount = getButtonCount(round)
+            setButtons(newTarget, newCount)
+            if (round >= 21) {
+            message.textContent = "🎉 Game Over!";
+            message.style.backgroundColor = "rgb(94, 179, 9)";
+            options.innerHTML = ''; 
+            return;
+            }
+
         })
     }
 }
+function statistic(){
 
-// btn.addEventListener('click', function () {
-//                 buttons.forEach(b => {
-//                 b.style.backgroundColor = getRandomRGB();
-//                 });
-//             });
+}
+
+let target = setTargetColor();
+let count = getButtonCount(round);
+setButtons(target, count);
