c7f69c06c579f24450fb852f5da7eaa32d769f3b
V.Kapanadze <vako.kapa@gmail.com>
2025-11-07 17:02:29 +0400

    details

--- Java Script 13/Classwork/app.js
+++ Java Script 13/Classwork/app.js
@@ -27,28 +27,45 @@
 let cont = document.querySelector(".cont")
 
        
-fetch("https://reqres.in/api/users")   
-.then(x => x.json())
-.then(x => randerUser(x.data))
+// fetch("https://reqres.in/api/users")   
+// .then(x => x.json())
+// .then(x => randerUser(x.data))
 
 
 
-function randerUser(arr) {
-    for(let obj of arr){
-      console.log(obj)
-      cont.innerHTML +=  `<div class="card" style="width: 18rem;">
-          <img src="${obj.avatar}" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">${obj.first_name} ${obj.last_name}</h5>
-            <p class="card-text">${obj.email}</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>`
-    }
+// function randerUser(arr) {
+//     for(let obj of arr){
+//       console.log(obj)
+//       cont.innerHTML +=  `<div class="card" style="width: 18rem;">
+//           <img src="${obj.avatar}" class="card-img-top" alt="...">
+//           <div class="card-body">
+//             <h5 class="card-title">${obj.first_name} ${obj.last_name}</h5>
+//             <p class="card-text">${obj.email}</p>
+//             <a href="#" class="btn btn-primary">Go somewhere</a>
+//           </div>
+//         </div>`
+//     }
+// }
+
+
+
+function renderUser(arr){
+  for (let el of arr){
+   cont.innerHTML +=
+    `<div class="card" style="width: 18rem;">
+      <img src="${el.imageUrl1}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${el.model} - ${el.brand}</h5>
+        <p class="card-text">ტრანსმისია - ${el.transmission}.</p>
+        <a href="./details.html?id=${el.id}" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>`
+  }
 }
 
 
+fetch("https://rentcar.stepprojects.ge/api/Car/filter?capacity=4&pageIndex=1&pageSize=10")
+.then(x => x.json())
+.then(x => renderUser(x.data))
 
-
-
-
+console.log(window.location.search)
