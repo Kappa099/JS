ed27ab98dc30d0e3f5b75d7ab52e9bf3af38c0ff
V.Kapanadze <vako.kapa@gmail.com>
2026-01-09 14:01:09 +0400

    minor fix3

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
-t.
+
+// function Greeting(name){
+//     return `Hello ${name}`
+// }
+// const user = prompt("What is your name")
+// document.write(Greeting(user))
+
+
+// function Warning(){
+//     return alert("This website will use your data")
+// }
+
+// Warning()
+// document.write("Welcome")
+
