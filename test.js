c71e6eef2e2f26fe66edda7250a204e50952b06f
V.Kapanadze <vako.kapa@gmail.com>
2026-01-10 13:15:19 +0400

    minor fix4

--- test.js
+++ test.js
@@ -13,3 +13,13 @@
 // Warning()
 // document.write("Welcome")
 
+function Func(arr, num){
+    let resp = false
+    if (arr.includes(num)){
+        resp = true
+    }
+    return resp
+}
+const arr = [1,2,3,4,5,6,7]
+const num = 3
+console.log(Func(arr, num))
